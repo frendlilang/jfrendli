c55d7badc7d926c46b599b8a227dfa30f1b4e9c6
ellej <81748770+elle-j@users.noreply.github.com>
2022-03-28 17:43:48 +0200

    Add Expression base and subclasses w/ visitor pattern

---
+++ src/dev/frendli/Expression.java
@@ -0,0 +1,75 @@
+package dev.frendli;
+
+// All expressions reside here as individual classes inheriting
+// from the "Expression" base class. Each subclass must implement
+// the "accept" method and call the corresponding "visit" method
+// on its visitor.
+
+public abstract class Expression {
+    public interface Visitor<R> {
+        R visitBinaryExpression(Binary expression);
+        R visitGroupingExpression(Grouping expression);
+        R visitLiteralExpression(Literal expression);
+        R visitUnaryExpression(Unary expression);
+    }
+
+    public abstract <R> R accept(Visitor<R> visitor);
+
+    public static class Binary extends Expression {
+        public final Expression left;
+        public final Token operator;
+        public final Expression right;
+
+        public Binary(Expression left, Token operator, Expression right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
+
+        @Override
+        public <R> R accept(Visitor<R> visitor) {
+            return visitor.visitBinaryExpression(this);
+        }
+    }
+
+    public static class Grouping extends Expression {
+        public final Expression expression;
+
+        public Grouping(Expression expression) {
+            this.expression = expression;
+        }
+
+        @Override
+        public <R> R accept(Visitor<R> visitor) {
+            return visitor.visitGroupingExpression(this);
+        }
+    }
+
+    public static class Literal extends Expression {
+        public final Object value;
+
+        public Literal(Object value) {
+            this.value = value;
+        }
+
+        @Override
+        public <R> R accept(Visitor<R> visitor) {
+            return visitor.visitLiteralExpression(this);
+        }
+    }
+
+    public static class Unary extends Expression {
+        public final Token operator;
+        public final Expression right;
+
+        public Unary(Token operator, Expression right) {
+            this.operator = operator;
+            this.right = right;
+        }
+
+        @Override
+        public <R> R accept(Visitor<R> visitor) {
+            return visitor.visitUnaryExpression(this);
+        }
+    }
+}
