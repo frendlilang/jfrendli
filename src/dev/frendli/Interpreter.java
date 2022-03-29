2f6557d7777400b438ceb3bbc63398290a935b3e
ellej <81748770+elle-j@users.noreply.github.com>
2022-03-29 18:02:48 +0200

    Evaluate binary expression in Interpreter

--- src/dev/frendli/Interpreter.java
+++ src/dev/frendli/Interpreter.java
@@ -7,26 +7,47 @@ package dev.frendli;
  */
 public class Interpreter implements Expression.Visitor<Object> {
     @Override
-    public Object visitLiteralExpression(Expression.Literal expression) {
-        return expression.value;
-    }
-
-    @Override
-    public Object visitUnaryExpression(Expression.Unary expression) {
+    public Object visitBinaryExpression(Expression.Binary expression) {
+        // Evaluate the operands left to right
+        Object left = evaluate(expression.left);
         Object right = evaluate(expression.right);
 
-        // Apply the operator to the right expression once the
-        // expression has been evaluated.
         switch (expression.operator.type) {
+            case EQUALS_WORD:
+                return isEqual(left, right);
+            case GREATER_THAN:
+                return (double)left > (double)right;
+            case GREATER_THAN_EQUALS:
+                return (double)left >= (double)right;
+            case LESS_THAN:
+                return (double)left < (double)right;
+            case LESS_THAN_EQUALS:
+                return (double)left <= (double)right;
             case MINUS:
-                // Case the right value to a double before applying
-                // the operator.
-                return -(double)right;
-            case NOT:
-                return !isTruthy(right);
+                return (double)left - (double)right;
+            case NOT_EQUALS:
+                return !isEqual(left, right);
+            case PLUS:
+                // Overload the + operator to allow for text concatenation
+                if (left instanceof Double && right instanceof Double) {
+                    return (double)left + (double)right;
+                }
+                if (left instanceof String && right instanceof String) {
+                    return (String)left + (String)right;
+                }
+                break;
+            case SLASH:
+                return (double)left / (double)right;
+            case STAR:
+                return (double)left * (double)right;
         }
 
         return null;
+    }
+
+    @Override
+    public Object visitLiteralExpression(Expression.Literal expression) {
+        return expression.value;
     }
 
     @Override
@@ -36,8 +57,47 @@ public class Interpreter implements Expression.Visitor<Object> {
         return evaluate(expression.expression);
     }
 
+    @Override
+    public Object visitUnaryExpression(Expression.Unary expression) {
+        Object right = evaluate(expression.right);
+
+        // Apply the operator to the right expression
+        // after the expression has been evaluated.
+        switch (expression.operator.type) {
+            case MINUS:
+                // Cast the right value to a double
+                // before applying the operator.
+                return -(double)right;
+            case NOT:
+                return !isTruthy(right);
+        }
+
+        return null;
+    }
+
     private Object evaluate(Expression expression) {
         return expression.accept(this);
+    }
+
+    /**
+     * Check if two objects are equal. (Null values and primitives of
+     * the same value are always equal; references are always not equal.)
+     *
+     * @param first The first object.
+     * @param second The second object.
+     * @return Whether they are equal.
+     */
+    private boolean isEqual(Object first, Object second) {
+        if (first == null && second == null) {
+            return true;
+        }
+        // Check if "first" is "null" so that the last
+        // line does not throw a NullPointerException
+        if (first == null) {
+            return false;
+        }
+
+        return first.equals(second);
     }
 
     /**
