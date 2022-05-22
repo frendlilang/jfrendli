d3187b4d8d0862f8a061cf40ab5d8ac51490c79c
ellej <81748770+elle-j@users.noreply.github.com>
2022-05-22 12:16:52 +0200

    Resolve 'create' statement and 'variable' expression

--- src/dev/frendli/Resolver.java
+++ src/dev/frendli/Resolver.java
@@ -16,28 +16,46 @@ import java.util.Stack;
  * any statements.
  */
 public class Resolver implements Expression.Visitor<Void>, Statement.Visitor<Void> {
+    private final ErrorReporter reporter;
     private final Interpreter interpreter;
     private final Stack<Set<String>> scopes = new Stack<>();   // Stack of local block scopes containing the variable names
 
-    public Resolver(Interpreter interpreter) {
+    public Resolver(Interpreter interpreter, ErrorReporter reporter) {
         this.interpreter = interpreter;
+        this.reporter = reporter;
     }
 
     @Override
     public Void visitBlockStatement(Statement.Block statement) {
         createScope();
-        resolve(statement.statements);
+        resolveBlock(statement.statements);
         discardScope();
 
         return null;
     }
 
+    @Override
+    public Void visitCreateStatement(Statement.Create statement) {
+        declare(statement.name);
+        verifyNotAccessingItselfInInitializer(statement.name, statement.initializer);
+        resolve(statement.initializer);
+
+        return null;
+    }
+
+    @Override
+    public Void visitVariableExpression(Expression.Variable expression) {
+        resolveLocalVariable(expression.name, expression);
+
+        return null;
+    }
+
     /**
-     * Resolve a list of statements.
+     * Resolve a block.
      *
      * @param statements The statements to resolve.
      */
-    private void resolve(List<Statement> statements) {
+    private void resolveBlock(List<Statement> statements) {
         for (Statement statement : statements) {
             resolve(statement);
         }
@@ -62,6 +80,31 @@ public class Resolver implements Expression.Visitor<Void>, Statement.Visitor<Voi
     }
 
     /**
+     * Resolve a local variable.
+     *
+     * @param name The variable token.
+     * @param expression The associated expression.
+     */
+    private void resolveLocalVariable(Token name, Expression expression) {
+        // Search for the name starting from the innermost scope.
+        for (int i = scopes.size() - 1; i >= 0; i--) {
+            if (scopes.get(i).contains(name.lexeme)) {
+                // Calculate the number of hops from the innermost scope
+                // to the closest scope where the name was defined.
+                int depth = scopes.size() - 1 - i;
+
+                // Add the result to the interpreter so that it can directly
+                // look up the environment in which the variable exists.
+                interpreter.resolve(expression, depth);
+                return;
+            }
+        }
+
+        // If this is reached, the variable is assumed to be global.
+        // (The interpreter will throw a runtime error if it is not.)
+    }
+
+    /**
      * Create a new scope.
      */
     private void createScope() {
@@ -73,5 +116,51 @@ public class Resolver implements Expression.Visitor<Void>, Statement.Visitor<Voi
      */
     private void discardScope() {
         scopes.pop();
+    }
+
+    /**
+     * Declare a name in the innermost scope.
+     *
+     * @param name The token to be declared.
+     */
+    private void declare(Token name) {
+        if (scopes.isEmpty()) {
+            return;
+        }
+
+        scopes.peek().add(name.lexeme);
+    }
+
+    /**
+     * Verify that the variable being declared is not also being
+     * accessed in its initializer.
+     *
+     * @param declared The declared variable.
+     * @param initialized The variable used in the initializer.
+     */
+    private void verifyNotAccessingItselfInInitializer(Token declared, Expression initialized) {
+        if (!(initialized instanceof Expression.Variable))
+            return;
+
+        // Accessing a variable that has previously been declared in
+        // an outer scope with the same name as the one currently being
+        // declared is not allowed in its own initializer. E.g:
+        // create x = 1
+        // if x > 0
+        //      create x = x        <-- illegal access on right-hand side
+        Token initializerName = ((Expression.Variable)initialized).name;
+        if (initializerName.lexeme.equals(declared.lexeme)) {
+            error(initializerName, "You cannot use '" + initializerName.lexeme + "' on both sides of '=' when creating it.");
+        }
+    }
+
+    /**
+     * Report an error.
+     *
+     * @param token The token that caused the error.
+     * @param message The error message.
+     */
+    private void error(Token token, String message) {
+        reporter.compileTimeError(token, message);
     }
 }
