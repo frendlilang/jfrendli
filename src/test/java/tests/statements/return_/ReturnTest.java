fbb10bc0dece63a9e0e8975cab2273b09c9b1835
ellej <81748770+elle-j@users.noreply.github.com>
2024-11-18 17:57:09 +0100

    Add more tests for 'return' statement.

--- src/test/java/tests/statements/return_/ReturnTest.java
+++ src/test/java/tests/statements/return_/ReturnTest.java
@@ -80,5 +80,72 @@ public class ReturnTest {
             String expected = "123";
             assertEquals(expected, actual);
         }
+
+        @Test
+        void itDoesNotExecuteStatementsAfterReturn() {
+            String sourceFile = "statements/return_/does-not-execute-statements-after-return.frendli";
+            String actual = run(sourceFile);
+            String expected = "";
+            assertEquals(expected, actual);
+        }
+    }
+
+    @Nested
+    public class ReturnTestExpectError extends FrendliTestExpectError {
+        @Test
+        void itCannotReturnFromTopLevel() {
+            String sourceFile = "statements/return_/error-return-from-top-level.frendli";
+            String actual = runExpectComptimeError(sourceFile);
+            String expected = """
+                    Error
+                      > Where:
+                         > Line 2 at 'return'
+                      > Message:
+                         > You can only return from within a definition.
+                    """;
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void itCannotReturnFromLocalNonFunction() {
+            String sourceFile = "statements/return_/error-return-from-local-non-function.frendli";
+            String actual = runExpectComptimeError(sourceFile);
+            String expected = """
+                    Error
+                      > Where:
+                         > Line 3 at 'return'
+                      > Message:
+                         > You can only return from within a definition.
+                    """;
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void itCannotReturnValueWhenNotUsingWithKeyword() {
+            String sourceFile = "statements/return_/error-return-value-not-using-with-keyword.frendli";
+            String actual = runExpectComptimeError(sourceFile);
+            String expected = """
+                    Error
+                      > Where:
+                         > Line 3 at '123'
+                      > Message:
+                         > You must add a new line after 'return'. To return with a value, use 'return with' instead.
+                    """;
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void itCannotReturnNoValueWhenUsingWithKeyword() {
+            String sourceFile = "statements/return_/error-return-no-value-using-with-keyword.frendli";
+            String actual = runExpectComptimeError(sourceFile);
+            String expected = """
+                    Error
+                      > Where:
+                         > Line 3 at the end of the line
+                      > Message:
+                         > You must add a valid expression after 'with' to return with that value. To not return an explicit value, use only 'return'.
+                    """;
+            assertEquals(expected, actual);
+        }
     }
 }
