956345d6d80f6ca18d3cfa6e0d58b9c0263e8a13
LJ <81748770+elle-j@users.noreply.github.com>
2024-12-02 12:42:51 +0100

    Add tests for function closures (#42)

--- src/test/java/tests/scope/block/BlockTest.java
+++ src/test/java/tests/scope/block/BlockTest.java
@@ -1,7 +1,7 @@
 package tests.scope.block;
 
-import tests.FrendliTestExpectError;
 import tests.FrendliTestExpectSuccess;
+import tests.FrendliTestExpectError;
 
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -36,8 +36,19 @@ public class BlockTest {
         }
 
         @Test
+        void itCanShadowParameterViaAnotherParameter() {
+            String sourceFile = "scope/block/shadow-parameter-via-parameter.frendli";
+            String actual = run(sourceFile);
+            String expected = """
+                    2
+                    1
+                    """.trim();
+            assertEquals(expected, actual);
+        }
+
+        @Test
         void itCanAccessLocalVariableInChildBlock() {
-            String sourceFile = "scope/block/access-local-variable-child-block.frendli";
+            String sourceFile = "scope/block/access-local-variable-in-child-block.frendli";
             String actual = run(sourceFile);
             String expected = """
                     parent
@@ -52,7 +63,7 @@ public class BlockTest {
     public class BlockTestExpectError extends FrendliTestExpectError {
         @Test
         void itCannotAccessVariableInDifferentScope() {
-            String sourceFile = "scope/block/error-access-variable-different-scope.frendli";
+            String sourceFile = "scope/block/error-access-variable-in-different-scope.frendli";
             String actual = runExpectComptimeError(sourceFile);
             String expected = """
                     Error
@@ -66,7 +77,7 @@ public class BlockTest {
 
         @Test
         void itCannotAccessVariableInSameBlockDifferentScope() {
-            String sourceFile = "scope/block/error-access-variable-same-block-different-scope.frendli";
+            String sourceFile = "scope/block/error-access-variable-in-same-block-different-scope.frendli";
             String actual = runExpectComptimeError(sourceFile);
             String expected = """
                     Error
@@ -74,6 +85,40 @@ public class BlockTest {
                          > Line 3 at 'a'
                       > Message:
                          > 'a' has not been created or defined. To create it, use 'create', or define it using 'define'.
+                    """;
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void itCannotAccessGlobalFunctionBeforeDeclaration() {
+            String sourceFile = "scope/block/error-access-global-function-before-declaration.frendli";
+            String actual = runExpectComptimeError(sourceFile);
+            String expected = """
+                    Error
+                      > Where:
+                         > Line 3 at 'myFunction2'
+                      > Message:
+                         > 'myFunction2' has not been created or defined. To create it, use 'create', or define it using 'define'.
+                    """;
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void itCannotEndBlockWithoutDedentation() {
+            String sourceFile = "scope/block/error-end-block-without-dedentation.frendli";
+            String actual = runExpectComptimeError(sourceFile);
+            String expected = """
+                    Error
+                      > Where:
+                         > Line 3 at the end of the file
+                      > Message:
+                         > Expected a new line.
+
+                    Error
+                      > Where:
+                         > Line 3 at the end of the file
+                      > Message:
+                         > Blocks must be dedented at the end. Add a new line and decrease the indentation level.
                     """;
             assertEquals(expected, actual);
         }
---
+++ src/test/java/tests/scope/closure/ClosureTest.java
@@ -0,0 +1,107 @@
+package tests.scope.closure;
+
+import tests.FrendliTestExpectSuccess;
+import tests.FrendliTestExpectError;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ClosureTest {
+    @Nested
+    public class ClosureTestExpectSuccess extends FrendliTestExpectSuccess {
+        @Test
+        void itCanCloseOverLocalVariable() {
+            String sourceFile = "scope/closure/close-over-local-variable.frendli";
+            String actual = run(sourceFile);
+            String expected = "local";
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void itCanCloseOverAndUpdateLocalVariable() {
+            String sourceFile = "scope/closure/close-over-and-update-local-variable.frendli";
+            String actual = run(sourceFile);
+            String expected = """
+                    1
+                    2
+                    3
+                    """.trim();
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void itCanCloseOverAndUpdateGlobalVariable() {
+            String sourceFile = "scope/closure/close-over-and-update-global-variable.frendli";
+            String actual = run(sourceFile);
+            String expected = """
+                    1
+                    2
+                    3
+                    101
+                    """.trim();
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void itCanCloseOverParameter() {
+            String sourceFile = "scope/closure/close-over-parameter.frendli";
+            String actual = run(sourceFile);
+            String expected = "argument";
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void itCanCloseOverNestedVariables() {
+            String sourceFile = "scope/closure/close-over-nested-variables.frendli";
+            String actual = run(sourceFile);
+            String expected = """
+                    globalA
+                    localB
+                    localC
+                    localD
+                    """.trim();
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void itCanResolveToSameVariableInStaticLexicalScope() {
+            String sourceFile = "scope/closure/resolve-to-same-variable-in-static-lexical-scope.frendli";
+            String actual = run(sourceFile);
+            String expected = """
+                    global
+                    global
+                    """.trim();
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void itCanChangeGlobalVariableToClosure() {
+            String sourceFile = "scope/closure/change-global-variable-to-closure.frendli";
+            String actual = run(sourceFile);
+            String expected = """
+                    empty
+                    local
+                    """.trim();
+            assertEquals(expected, actual);
+        }
+    }
+
+    @Nested
+    public class ClosureTestExpectError extends FrendliTestExpectError {
+        @Test
+        void itCannotAccessClosedOverVariableInDifferentScope() {
+            String sourceFile = "scope/closure/error-access-closed-over-variable-in-different-scope.frendli";
+            String actual = runExpectComptimeError(sourceFile);
+            String expected = """
+                    Error
+                      > Where:
+                         > Line 11 at 'a'
+                      > Message:
+                         > 'a' has not been created or defined. To create it, use 'create', or define it using 'define'.
+                    """;
+            assertEquals(expected, actual);
+        }
+    }
+}
